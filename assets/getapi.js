20d97acac33cc64725b06318c689425a7855c08d
Dan <danlevy4@gmail.com>
2021-03-21 14:01:35 +0200

    design WIP

--- assets/getapi.js
+++ assets/getapi.js
@@ -129,11 +129,9 @@ function alert(message){
 
 
 function show(){
-  document.getElementById("spinner-back").classList.add("show");
   document.getElementById("spinner-front").classList.add("show");
 }
 function hide(){
-  document.getElementById("spinner-back").classList.remove("show");
   document.getElementById("spinner-front").classList.remove("show");
 }
 
